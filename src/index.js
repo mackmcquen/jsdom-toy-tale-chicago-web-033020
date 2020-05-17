fa5e72285339ccb64f4c03c4f7fde1c0c6c8836b
mackmcquen <mackmcquen@comcast.net>
2020-05-17 14:22:47 -0500

    Progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+function main() {
+fetchToys()
+renderToys()
+}
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  
 });
+
+let toyCollection = document.querySelector('#toy-collection')
+
+function fetchToys() {
+  return fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(toys => renderToys(toys))
+}
+
+
+
+main()
